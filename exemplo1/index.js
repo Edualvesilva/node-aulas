105fa830df9c3bc157e2fa5888d36805def724ab
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-09-13 08:36:52 -0300

    Initial commit

---
+++ exemplo1/index.js
@@ -0,0 +1,16 @@
+let aluno = "Kauan";
+let nota1 = 7.8;
+let nota2 = 6.4;
+
+let media = (nota1 + nota2) / 2;
+console.log(media);
+console.log("-----");
+
+const dados = {
+    nome : "Manoel Gomes",
+    idade : 19,
+    signo: "Áries"
+};
+
+console.log(dados.idade);
+console.table(dados);
