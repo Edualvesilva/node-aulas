78afc79044ce792767bcbf658039b759baa169b1
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-09-18 10:30:48 -0300

    Making a middleare for 404

--- exercicio2/app.js
+++ exercicio2/app.js
@@ -12,6 +12,11 @@ app.get('/',(req,res) => {
     res.render('paginas/404');
  });
 
+ // Captura todas rotas 
+ app.use((req, res) => {
+   res.status(404).render('paginas/404');
+});
+
  app.listen(6300,() => {
     console.log('Servidor expressJS rodando normalmente')
 })
